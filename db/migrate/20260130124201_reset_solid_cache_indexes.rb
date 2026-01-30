c5d0ef262c479694d469bc91cb6855562937244a
ryo-ri-16 <ujiro1998@gmail.com>
2026-01-30 21:46:00 +0900

    Fix solid_cache index and production build command

--- db/migrate/20260130124201_reset_solid_cache_indexes.rb
+++ db/migrate/20260130124201_reset_solid_cache_indexes.rb
@@ -1,11 +1,12 @@
 class ResetSolidCacheIndexes < ActiveRecord::Migration[8.0]
   def change
     remove_index :solid_cache_entries,
-                  column: [ :key_hash, :shard ],
-                  if_exists: true
+                 column: [ :key_hash, :shard ],
+                 if_exists: true
+
+    add_index :solid_cache_entries,
+              [ :key_hash, :shard ],
+              unique: true,
+              name: :index_solid_cache_entries_on_key_hash_and_shard
   end
-  add_index :solid_cache_entries,
-          [ :key_hash, :shard ],
-          unique: true,
-          name: :index_solid_cache_entries_on_key_hash_and_shard
 end
