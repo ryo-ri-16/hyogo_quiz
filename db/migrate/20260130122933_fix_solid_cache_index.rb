b876f3ef5dfb0f46730942d025e858c0c814ee5e
ryo-ri-16 <ujiro1998@gmail.com>
2026-01-30 21:34:13 +0900

    Fix solid_cache unique index duplication

--- db/migrate/20260130122933_fix_solid_cache_index.rb
+++ db/migrate/20260130122933_fix_solid_cache_index.rb
@@ -1,6 +1,9 @@
 class FixSolidCacheIndex < ActiveRecord::Migration[8.0]
   def change
-    remove_index :solid_cache_entries, column: [ :key_hash, :shard ]
+    # 余計な方を明示的に消す
+    remove_index :solid_cache_entries, name: "index_solid_cache_entries_unique"
+
+    # 正式名称で一本に統一
     add_index :solid_cache_entries,
               [ :key_hash, :shard ],
               unique: true,
